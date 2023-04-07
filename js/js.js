b64c0b4cd1489e5f7bd8cb0ebbdc905d73478378
Isley Theodoro <110562962+isleyTheodoro@users.noreply.github.com>
2023-04-06 23:30:44 -0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,6 @@
+const myModalEl = document.querySelector('#modal-body')
+
+const modal = new bootstrap.Modal(modal-bodyEl) // initialized with defaults
+
+const configObject = { keyboard: false }
+const modal1 = new bootstrap.Modal(modal-bodyEl, configObject) // initialized with no keyboard
